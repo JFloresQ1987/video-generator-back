c43fdf88f4e296f6f85b916b5d46e690f9ed337d
Jorge <desarrollador117_usi@minedu.gob.pe>
2024-05-09 23:16:35 -0500

    se mejora servicio de pago con stripe

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -4,10 +4,11 @@ import { STRIPE_PRIVATE_KEY } from "../config.js";
 const stripe = new Stripe(STRIPE_PRIVATE_KEY);
 
 export const createSession = async (req, res) => {
-  try {    
-    
-    const item_id = req.body.items[0].id;
-    
+  try {
+
+    // const item_id = req.body.items[0].id;
+    const item_id = req.body.id;
+
     const items = req.body.items.map((item) => {
       return {
         price_data: {
@@ -18,10 +19,10 @@ export const createSession = async (req, res) => {
           },
           unit_amount: item.price * 100,
         },
-        quantity: item.qty
+        quantity: 1//item.qty
       }
     });
-  
+
     const session = await stripe.checkout.sessions.create({
       line_items: [...items],
       metadata: {
@@ -33,36 +34,6 @@ export const createSession = async (req, res) => {
       // cancel_url: `${YOUR_DOMAIN}/cancel.html`,
       cancel_url: `http://localhost:4200/cancel`,
     });
-
-    // const session = await stripe.checkout.sessions.create({
-    //   line_items: [
-    //     {
-    //       price_data: {
-    //         product_data: {
-    //           name: "Laptop",
-    //         },
-    //         currency: "usd",
-    //         unit_amount: 2000,
-    //       },
-    //       quantity: 1,
-    //     },
-    //     {
-    //       price_data: {
-    //         product_data: {
-    //           name: "TV",
-    //         },
-    //         currency: "usd",
-    //         unit_amount: 1000,
-    //       },
-    //       quantity: 2,
-    //     },
-    //   ],
-    //   mode: "payment",
-    //   success_url: "http://localhost:3000/success",
-    //   cancel_url: "http://localhost:3000/cancel",
-    // });
-
-    // console.log(session);
     return res.json({ url: session.url });
   } catch (error) {
     return res.status(500).json({ message: error.message });

171ed41cbbd306c7ee1cf18bf40a8709dbe83616
Jorge <desarrollador117_usi@minedu.gob.pe>
2024-05-29 16:42:07 -0500

    prueba de despliegue continuo

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,124 +1,124 @@
-import Stripe from "stripe";
-import { STRIPE_PRIVATE_KEY, WEBHOOK_END_POINT_SECRET } from "../config.js";
-import { supabase, supabaseAdmin } from '../libs/supabase.js';
+// import Stripe from "stripe";
+// import { STRIPE_PRIVATE_KEY, WEBHOOK_END_POINT_SECRET } from "../config.js";
+// import { supabase, supabaseAdmin } from '../libs/supabase.js';
 
-const stripe = new Stripe(STRIPE_PRIVATE_KEY);
+// const stripe = new Stripe(STRIPE_PRIVATE_KEY);
 
-export const createSession = async (req, res) => {
-  try {
-
-    // const item_id = req.body.items[0].id;
-    const item_id = req.body.id;
-
-    const items = req.body.items.map((item) => {
-      return {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: item.title,
-            images: [item.image]
-          },
-          unit_amount: item.price * 100,
-        },
-        quantity: 1//item.qty
-      }
-    });
-
-    const session = await stripe.checkout.sessions.create({
-      line_items: [...items],
-      metadata: {
-        order_id: item_id,
-      },
-      mode: 'payment',
-      // success_url: `${YOUR_DOMAIN}/success.html`,
-      success_url: `http://localhost:4200/sales/${item_id}`,
-      // cancel_url: `${YOUR_DOMAIN}/cancel.html`,
-      cancel_url: `http://localhost:4200/cancel`,
-    });
-    return res.json({ url: session.url });
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-};
-
-
-
-
-
-// const stripe = require('stripe')('sk_test_...');
-// const express = require('express');
-// const app = express();
-
-
-// This is your Stripe CLI webhook secret for testing your endpoint locally.
-// const endpointSecret = "whsec_f456e03fafdd597ec5a873baa6280efc000d2e5b73d6227ac6da984f16ae225b";
-
-// app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-// export const webhook = async (req, res) => {
-
-//   console.log('entrooooo')
-
+// export const createSession = async (req, res) => {
 //   try {
-//     const sig = req.headers['stripe-signature'];
 
-//     let event;
+//     // const item_id = req.body.items[0].id;
+//     const item_id = req.body.id;
 
-//     try {
-//       console.log('paso 1')
-//       console.log(req.body)
-//       console.log(sig)
-//       console.log(WEBHOOK_END_POINT_SECRET)
-//       event = stripe.webhooks.constructEvent(req.body, sig, WEBHOOK_END_POINT_SECRET);
-//     } catch (err) {
-//       console.log(err.message)
-//       res.status(400).send(`Webhook Error: ${err.message}`);
-//       return;
-//     }
+//     const items = req.body.items.map((item) => {
+//       return {
+//         price_data: {
+//           currency: 'usd',
+//           product_data: {
+//             name: item.title,
+//             images: [item.image]
+//           },
+//           unit_amount: item.price * 100,
+//         },
+//         quantity: 1//item.qty
+//       }
+//     });
 
-//     // Handle the event
-//     switch (event.type) {
-//       case 'payment_intent.succeeded':
-//         const paymentIntentSucceeded = event.data.object;
-//         // Then define and call a function to handle the event payment_intent.succeeded
-//         console.log('payment_intent.succeeded')
-//         console.log(paymentIntentSucceeded);
-
-
-//         console.log(paymentIntentSucceeded.customer_details)
-//         const user_email = paymentIntentSucceeded.customer_details?.email;
-
-//         // const id = 'qwe';
-//         console.log(paymentIntentSucceeded.metadata)
-//         const id = paymentIntentSucceeded.metadata?.order_id;
-
-//         const { error } = await supabaseAdmin
-//           .from('orders')
-//           .update({
-//             // order_state: 'edited',
-//             user_email: user_email,
-//             payment_state: 'paid',
-//             // images: _images,
-//             // video_rendered_url: _url,
-//           })
-//           .eq('id', id)
-
-//         // if (data) console.log(data)
-//         if (error) console.log(error)
-
-//         break;
-//       // ... handle other event types
-//       default:
-//         console.log(`Unhandled event type ${event.type}`);
-//     }
-//     console.log('llego');
-//     // Return a 200 response to acknowledge receipt of the event
-//     res.send();
-//     // return res.json(null);
-//     // return res.json({ url: session.url });
+//     const session = await stripe.checkout.sessions.create({
+//       line_items: [...items],
+//       metadata: {
+//         order_id: item_id,
+//       },
+//       mode: 'payment',
+//       // success_url: `${YOUR_DOMAIN}/success.html`,
+//       success_url: `http://localhost:4200/sales/${item_id}`,
+//       // cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+//       cancel_url: `http://localhost:4200/cancel`,
+//     });
+//     return res.json({ url: session.url });
 //   } catch (error) {
-//     console.log(error);
 //     return res.status(500).json({ message: error.message });
 //   }
 // };
 
-// app.listen(4242, () => console.log('Running on port 4242'));
+
+
+
+
+// // const stripe = require('stripe')('sk_test_...');
+// // const express = require('express');
+// // const app = express();
+
+
+// // This is your Stripe CLI webhook secret for testing your endpoint locally.
+// // const endpointSecret = "whsec_f456e03fafdd597ec5a873baa6280efc000d2e5b73d6227ac6da984f16ae225b";
+
+// // app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+// // export const webhook = async (req, res) => {
+
+// //   console.log('entrooooo')
+
+// //   try {
+// //     const sig = req.headers['stripe-signature'];
+
+// //     let event;
+
+// //     try {
+// //       console.log('paso 1')
+// //       console.log(req.body)
+// //       console.log(sig)
+// //       console.log(WEBHOOK_END_POINT_SECRET)
+// //       event = stripe.webhooks.constructEvent(req.body, sig, WEBHOOK_END_POINT_SECRET);
+// //     } catch (err) {
+// //       console.log(err.message)
+// //       res.status(400).send(`Webhook Error: ${err.message}`);
+// //       return;
+// //     }
+
+// //     // Handle the event
+// //     switch (event.type) {
+// //       case 'payment_intent.succeeded':
+// //         const paymentIntentSucceeded = event.data.object;
+// //         // Then define and call a function to handle the event payment_intent.succeeded
+// //         console.log('payment_intent.succeeded')
+// //         console.log(paymentIntentSucceeded);
+
+
+// //         console.log(paymentIntentSucceeded.customer_details)
+// //         const user_email = paymentIntentSucceeded.customer_details?.email;
+
+// //         // const id = 'qwe';
+// //         console.log(paymentIntentSucceeded.metadata)
+// //         const id = paymentIntentSucceeded.metadata?.order_id;
+
+// //         const { error } = await supabaseAdmin
+// //           .from('orders')
+// //           .update({
+// //             // order_state: 'edited',
+// //             user_email: user_email,
+// //             payment_state: 'paid',
+// //             // images: _images,
+// //             // video_rendered_url: _url,
+// //           })
+// //           .eq('id', id)
+
+// //         // if (data) console.log(data)
+// //         if (error) console.log(error)
+
+// //         break;
+// //       // ... handle other event types
+// //       default:
+// //         console.log(`Unhandled event type ${event.type}`);
+// //     }
+// //     console.log('llego');
+// //     // Return a 200 response to acknowledge receipt of the event
+// //     res.send();
+// //     // return res.json(null);
+// //     // return res.json({ url: session.url });
+// //   } catch (error) {
+// //     console.log(error);
+// //     return res.status(500).json({ message: error.message });
+// //   }
+// // };
+
+// // app.listen(4242, () => console.log('Running on port 4242'));
--- src/routes/supabase.routes.js
+++ src/routes/supabase.routes.js
@@ -1,39 +1,39 @@
-import { Router } from "express";
-import expressFileupload from "express-fileupload";
-import {
-    getCategoriesAll,
-    getProductsByCategoryId,
-    getModelsByProductId,
-    getModelById,
-    getOrderById,
-    createOrder,
-    updateOrder,
-    fileUpload
-} from "../controllers/supabase.controller.js";
+// import { Router } from "express";
+// import expressFileupload from "express-fileupload";
+// import {
+//     getCategoriesAll,
+//     getProductsByCategoryId,
+//     getModelsByProductId,
+//     getModelById,
+//     getOrderById,
+//     createOrder,
+//     updateOrder,
+//     fileUpload
+// } from "../controllers/supabase.controller.js";
 
-const router = Router();
-router.use(expressFileupload());
+// const router = Router();
+// router.use(expressFileupload());
 
-// router.post("/create-checkout-session", createSession);
+// // router.post("/create-checkout-session", createSession);
 
-// router.get("/success", (req, res) => res.redirect("/success.html"));
+// // router.get("/success", (req, res) => res.redirect("/success.html"));
 
-// router.get("/cancel", (req, res) => res.redirect("/cancel.html"));
-// router.get('/', [], getProductsAll);
-// router.get('/:category_id', getProductsByCategoryId);
-router.get('/categories-all', getCategoriesAll);
-router.get('/products-by-category/:category_id', getProductsByCategoryId);
-router.get('/models-by-product/:product_id', getModelsByProductId);
-router.get('/model-by-id/:id', getModelById);
-router.get('/order-by-id/:id', getOrderById);
-// router.get('qwe/:category_id', [], getProductsByCategoryId);
+// // router.get("/cancel", (req, res) => res.redirect("/cancel.html"));
+// // router.get('/', [], getProductsAll);
+// // router.get('/:category_id', getProductsByCategoryId);
+// router.get('/categories-all', getCategoriesAll);
+// router.get('/products-by-category/:category_id', getProductsByCategoryId);
+// router.get('/models-by-product/:product_id', getModelsByProductId);
+// router.get('/model-by-id/:id', getModelById);
+// router.get('/order-by-id/:id', getOrderById);
+// // router.get('qwe/:category_id', [], getProductsByCategoryId);
 
-router.post("/orders", createOrder);
-router.put("/orders", updateOrder);
+// router.post("/orders", createOrder);
+// router.put("/orders", updateOrder);
 
-// router.put('/:id', validarJWT, fileUpload);
-router.post('/orders/images', fileUpload);
+// // router.put('/:id', validarJWT, fileUpload);
+// router.post('/orders/images', fileUpload);
 
-// router.post("/create-checkout-session", createSession);
+// // router.post("/create-checkout-session", createSession);
 
-export default router;
+// export default router;
